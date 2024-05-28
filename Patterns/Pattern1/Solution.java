57c5f7315672a6198ff1e87c3ac08c3aef7b3ce9
PranayVirat <virat@deepu.lan>
2024-05-28 19:11:36 -0400

    Updated Pattern1

--- Patterns/Pattern1/Solution.java
+++ Patterns/Pattern1/Solution.java
@@ -4,11 +4,9 @@ class Solution {
         // code here
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(j==n-1) System.out.print("*");
-                else
                 System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
