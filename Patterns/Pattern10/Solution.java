54307936998457a3a1a01fd1e88e2fc7d616a6c1
PranayVirat <virat@deepu.lan>
2024-05-28 20:16:12 -0400

    Added Pattern10

---
+++ Patterns/Pattern10/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
